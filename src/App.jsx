8f58a5a5064402939e163706d9f2cb8f968cb2c5
Tafsirchy <tafsirchy1000@gmail.com>
2026-01-05 19:43:32 +0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Home from './pages/Home';
+import SmoothScroll from './components/SmoothScroll';
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </AnimatePresence>
+      <SmoothScroll>
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </AnimatePresence>
+      </SmoothScroll>
     </Router>
   );
 }
---
+++ src/components/SmoothScroll.jsx
@@ -0,0 +1,15 @@
+import { ReactLenis } from 'lenis/react';
+
+const SmoothScroll = ({ children }) => {
+  return (
+    <ReactLenis root options={{ 
+      lerp: 0.1, 
+      duration: 1.5, 
+      smoothWheel: true 
+    }}>
+      {children}
+    </ReactLenis>
+  );
+};
+
+export default SmoothScroll;
